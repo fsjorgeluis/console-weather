3eb3b00641f65f3ef5a20f64c45c8ce1b7fd8307
Jorge Fernandez <fsjorgeluis@gmail.com>
2022-04-01 02:17:43 -0400

    feat: :construction: Read user input and prepare to make a request to weather api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
-const { inquirerMenu, pauseMenu } = require("./libs/inquirer");
 require("colors")
+
+const { inquirerMenu, pauseMenu, readInput } = require("./libs/inquirer");
+const Searches = require("./services/Searches");
+
 
 const main = async () => {
     let opt;
+
+    const searches = new Searches();
 
     do {
         opt = await inquirerMenu();
 
         switch (opt) {
             case 1:
-                console.log(opt)
+                // show message
+                const city = await readInput('City: ');
+                console.log(city)
+
+                // search places
+
+                // select city
+
+                // get weather info
+
+                // show results
+                console.log('\nCity information\n'.green);
+                console.log('City:',);
+                console.log('Latitude:',);
+                console.log('Longitude:',);
+                console.log('Temperature:',);
+                console.log('Min Temp:',);
+                console.log('Max Temp:',);
+
                 break;
     
             case 2:
-                console.log(opt)
+                console.log(searches.history);
                 break;
         
             case 0:
---
+++ src/services/Searches.js
@@ -0,0 +1,20 @@
+
+
+class Searches {
+
+    history = ['Madrid', 'Nueva Esparta', 'Ottawa', 'Newark'];
+
+    constructor() {
+        // TODO: read data from db
+    }
+
+    async city(place = '') {
+        // TODO: fetch city api
+        console.log(place);
+
+        // return array of cities that matches place arg 
+        return [];
+    }
+}
+
+module.exports = Searches;
